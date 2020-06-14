065788ab15d458079aee97a1de2c992c1879f45a
孤独患者 <1101278643@qq.com>
2020-06-14 16:13:55 +0800

    asidemenugroup样式剥离

--- packages/components/aside-menu-group/AsideMenuGroup.vue
+++ packages/components/aside-menu-group/AsideMenuGroup.vue
@@ -15,19 +15,3 @@ export default {
   name: "MAsideMenuGroup",
 }
 </script>
-
-<style lang="scss">
-  .aside-Menu-group {
-    color: inherit;
-    padding: 10px 0;
-    .aside-Menu-group-header {
-      height: 48px;
-      line-height: 48px;
-      padding: 0 40px;
-      .line {
-        border: 2px solid rgba($color: $ui, $alpha: 1);
-        margin-right: 6px;
-      }
-    }
-  }
-</style>
