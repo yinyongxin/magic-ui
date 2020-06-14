d9ae26398abca66843f5d819de871ba86a6324f9
孤独患者 <1101278643@qq.com>
2020-06-14 16:12:26 +0800

    asidemenu样式剥离

--- packages/components/aside-menu/AsideMenu.vue
+++ packages/components/aside-menu/AsideMenu.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- <div class="aside-menu" > -->
     <div 
-      class="aside-menu"
       :class="[
+        'aside-menu',
         {
         'aside-menu-border-right': isBorder == 'right'? true : false,
         'aside-menu-border-left': isBorder == 'left'? true : false,
@@ -26,10 +26,8 @@ export default {
   data() {
     return {
       activeIndex: '',
-      // submenuitems: null
     }
   },
-  // inject: ['submenuitems'],
   provide() {
     return {
       rootMenu: this
@@ -65,30 +63,7 @@ export default {
       default: false
     }
   },
-
   mounted() {
-    // console.log('所有子组件', this.$children)
-    // console.log(this.submenuitems)
-    // this.submenuitems = this.submenuitems
-    // this.$children.forEach(item => {
-    //   if(item.$children.length > 0) console.log('还有子组件')
-    //   if(item.index) console.log('子组件index',item.index)
-    // }) 
   }
 }
 </script>
-
-<style lang="scss">
-.aside-menu {
-  color: $title-color;
-  height: 100%;
-  background-color: #fff;
-  overflow-y: auto;
-}
-.aside-menu-border-right {
-  border-right: 1px solid #DCDFE6;
-}
-.aside-menu-border-left {
-  border-left: 1px solid #DCDFE6;
-}
-</style>
